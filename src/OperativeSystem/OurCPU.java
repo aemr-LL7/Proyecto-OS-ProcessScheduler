04ad0e8699f3aa139c5bb093756333e3cc0fd63c
eros <villarroel.eros@correo.unimet.edu.ve>
2025-02-16 22:19:02 -0400

    WASAAAAAA PARTE 6

--- src/OperativeSystem/OurCPU.java
+++ src/OperativeSystem/OurCPU.java
@@ -62,7 +62,6 @@ public class OurCPU extends Thread {
                     } // Verificar si el proceso está bloqueado (I/O-bound)
                     else if (currentProcess.getPcb().getState() == ProcessState.BLOCKED) {
                         System.out.println("\nCPU detectó que el proceso " + currentProcess.getPcb().getName() + " esta bloqueado");
-//                        OperatingSystem.getInstance().getExceptionHandler().interruptCPU(this); //CODIGO VIEJO
 
                     }
 
